9eecdd17db810c25ef234e759336177670cb5c9f
Troels Sheepyhead Jessen <kairyuka@gmail.com>
2019-02-12 18:35:25 +0100

    Initial commit with functional winmain and build script

---
+++ src/windows_main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int WinMain(
+    HINSTANCE instance,     // Handle to the current instance of the application
+    HINSTANCE prevInstance, // Handle to the previous instance of the application, is always NULL
+    LPSTR cmdLine,          // Command line for the application, excluding the program name
+    int showCmd             // Unknown, probably useless
+)
+{
+    return 0;
+}
